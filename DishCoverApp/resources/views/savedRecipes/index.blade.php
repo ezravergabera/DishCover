560a3136dfb748c390e5042841b1e8754ad27191
Jose Ezra Nazarene Vergabera <120149324+ezravergabera@users.noreply.github.com>
2024-07-07 03:13:09 +0800

    created a display for saved recipes of the user

---
+++ DishCoverApp/resources/views/savedRecipes/index.blade.php
@@ -0,0 +1,49 @@
+@extends('layouts.layout')
+
+@section('title', 'DishCover')
+
+@section('style', 'css/recipe.css')
+
+@section('header')
+    @include('components.headerWithAuth')
+@endsection
+
+@section('content')
+    {{-- <div class="input-wrapper">
+        <input type="text" class="form-control" placeholder="Search...">
+        <button class="btn btn-outline-secondary" type="button"><i class="fas fa-search"></i></button>
+        <button class="btn btn-outline-secondary" type="button"><img src="{{asset('svg/ingredients-filter.svg')}}" height="20px"/></button>
+        <button class="btn btn-outline-secondary" type="button"><img src="{{asset('images/dishcover-3-1.png')}}" height="20px"/></button>
+    </div> --}}
+
+    <h1 class="search-results-label">Saved Recipes for {{Auth::user()->name}}</h1>
+
+    @if($recipes->count()==0)
+        <h3>No recipes saved yet.</h3>
+    @else
+        <div class="container">
+            <div class="row">
+                @foreach($recipes as $recipe)
+                    <div class="col-xxl-5th col-md-6 col-lg-4 col-xl-3 mb-4">
+                        <div class="card recipe">
+                            <div class="image-container">
+                                @if($recipe->recipe_image)
+                                    <img src="{{ $recipe->recipe_image }}" class="card-img-top" alt="{{ $recipe->recipe_label }}">
+                                @endif
+                                <div class="overlay">
+                                    <a href="{{ $recipe->recipe_url }}" target="_blank" class="button">View</a>
+                                    <button class="button">Remove Recipe</button>
+                                </div>
+                            </div>
+                            <div class="card-body">
+                                <h5 class="card-title">{{ $recipe->recipe_label }}</h5>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+        </div>
+    @endif
+
+    <link rel="stylesheet" href="{{ asset('css/recipe.css') }}">
+@endsection
