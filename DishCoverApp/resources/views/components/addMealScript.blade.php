a8fe8903a8ee7e6e41c62ca62bee338bc37d8921
Jose Ezra Nazarene Vergabera <120149324+ezravergabera@users.noreply.github.com>
2025-01-14 03:01:33 +0800

    Added functions

--- DishCoverApp/resources/views/components/addMealScript.blade.php
+++ DishCoverApp/resources/views/components/addMealScript.blade.php
@@ -1,4 +1,50 @@
 <script>
+    function decrementWeek() {
+        const weekLabel = document.getElementById('weekID');
+        const currentText = weekLabel.textContent.trim();
+        const currentWeek = parseInt(currentText.replace('Week ', ''));
+        if (!isNaN(currentWeek)) {
+            weekLabel.textContent = `Week ${currentWeek - 1}`;
+        }
+    }
+
+    function incrementWeek() {
+        const weekLabel = document.getElementById('weekID');
+        const currentText = weekLabel.textContent.trim();
+        const currentWeek = parseInt(currentText.replace('Week ', ''));
+        if (!isNaN(currentWeek)) {
+            weekLabel.textContent = `Week ${currentWeek + 1}`;
+        }
+    }
+
+    document.addEventListener('DOMContentLoaded', () => {
+        const prevButton = document.querySelector('.custom-prev-btn');
+        const nextButton = document.querySelector('.custom-next-btn');
+
+        nextButton.addEventListener('click', incrementWeek);
+
+        const carouselElement = document.getElementById('carouselExample');
+        const items = carouselElement.querySelectorAll('.carousel-item');
+        
+        carouselElement.addEventListener('slid.bs.carousel', (event) => {
+            prevButton.addEventListener('click', decrementWeek);
+            nextButton.addEventListener('click', incrementWeek);
+            const activeIndex = event.to;
+            const lastIndex = items.length - 1;
+            
+            if (activeIndex === lastIndex) {
+                nextButton.removeEventListener('click', incrementWeek);
+                prevButton.addEventListener('click', decrementWeek);
+            } 
+            
+            if (activeIndex == 0) {
+                prevButton.removeEventListener('click', decrementWeek);
+                nextButton.addEventListener('click', incrementWeek);
+            }
+        });
+
+    });
+
     function createModalShow() {
         var statusModal = new bootstrap.Modal(document.getElementById('statusModal'));
         statusModal.show();
