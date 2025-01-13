c9a5996287a6896d77a3d40888cfe1753fe47cab
Ramondroid <116806656+Ramondroid@users.noreply.github.com>
2025-01-13 16:11:17 +0800

    Profile Updated
    Rami Update

--- DishCoverApp/resources/views/profile/partials/delete-user-form.blade.php
+++ DishCoverApp/resources/views/profile/partials/delete-user-form.blade.php
@@ -9,10 +9,10 @@
         </p>
     </header>
 
-    <x-danger-button
+    <div class="button-container"><button class="delete-button"
         x-data=""
         x-on:click.prevent="$dispatch('open-modal', 'confirm-user-deletion')"
-    >{{ __('Delete Account') }}</x-danger-button>
+    >{{ __('Delete Account') }}</button></div>
 
     <x-modal name="confirm-user-deletion" :show="$errors->userDeletion->isNotEmpty()" focusable>
         <form method="post" action="{{ route('profile.destroy') }}" class="p-6">
