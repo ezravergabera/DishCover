9283a66c4565f1668addf3f79a6c5f6da6c5bb9d
Ramondroid <116806656+Ramondroid@users.noreply.github.com>
2025-01-13 16:46:20 +0800

    Full Profile Update
    Yup

--- DishCoverApp/resources/views/profile/partials/delete-user-form.blade.php
+++ DishCoverApp/resources/views/profile/partials/delete-user-form.blade.php
@@ -19,9 +19,9 @@
             @csrf
             @method('delete')
 
-            <h2 class="text-lg font-medium text-gray-900">
-                {{ __('Are you sure you want to delete your account?') }}
-            </h2>
+            <div class="delete-account-container"><h2 class="text-lg font-medium text-gray-900">{{ __('Are you sure you want to delete your account?') }}
+            </h2></div> 
+                
 
             <p class="mt-1 text-sm text-gray-600">
                 {{ __('Once your account is deleted, all of its resources and data will be permanently deleted. Please enter your password to confirm you would like to permanently delete your account.') }}
@@ -41,14 +41,14 @@
                 <x-input-error :messages="$errors->userDeletion->get('password')" class="mt-2" />
             </div>
 
-            <div class="mt-6 flex justify-end">
-                <x-secondary-button x-on:click="$dispatch('close')">
+            <div class="delete-button-container">
+            <div class="button-container"><button class=""custom-button"" x-on:click="$dispatch('close')">
                     {{ __('Cancel') }}
-                </x-secondary-button>
+                </button></div>
 
-                <x-danger-button class="ms-3">
+                <div class="button-container"><button class="delete-button">
                     {{ __('Delete Account') }}
-                </x-danger-button>
+                </button></div>
             </div>
         </form>
     </x-modal>
